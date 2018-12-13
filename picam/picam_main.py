7ab267a9eb4e71d865f26ded626a2de6dcfc893d
AceTwin <11320027+AceTwin@users.noreply.github.com>
2018-12-13 23:36:01 +0000

    Picam done

---
+++ picam/picam_main.py
@@ -0,0 +1,55 @@
+from picamera import PiCamera
+import paho.mqtt.client as mqtt
+import requests
+import json
+uri = 'http://192.168.3.1:3000/hooks/r9CNJs8hpYKxe53np/yNvsbryGk4YJGfcptALtHAj6aJKofXz9Z5MdhH2g7cdYQSnw'
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("pressure/sensor")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(str(msg.payload)[2])
+    if str(msg.payload)[2]=="y":
+        #The door said something
+        print("The Door!!!")
+        imagepost()
+    else:
+        #And it's gone
+        print("I release you!")
+
+def imagepost():
+    camera = PiCamera()
+    camera.capture('image.jpg')
+    data={}
+    data = {
+        "attachments": [
+            {
+                "text":"Hello",
+                "image_url":"http://192.168.3.44:8000/image.jpg",
+                "color":"#764FA5"
+            }
+        ]
+    }
+
+    r = requests.post(uri, json.dumps(data)).content
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("192.168.3.1", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
