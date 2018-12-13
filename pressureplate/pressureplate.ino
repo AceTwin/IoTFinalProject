8b3f62ffaf2bac65e46ddcc67506bcad9d623684
AceTwin <cara.cornel@byu.edu>
2018-12-13 13:05:19 -0700

    Fixed the comments

--- pressureplate/pressureplate.ino
+++ pressureplate/pressureplate.ino
@@ -3,17 +3,19 @@
 //Here is an example of code that you could use with this switch.
 
 //numbers are according to the GPIO layout: https://wiki.wemos.cc/products:d1:d1_mini
-const int buttonPin=0; //D3
+const int buttonPin=0; //D3 -- this is the input for the pressure plate
 const int ledPin =2; //D4 -- this is just the light on the arduino.
+
+//inputs for pushbutton in Ground and D3
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);     
+  pinMode(ledPin, OUTPUT);     //LED on the arduino
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT_PULLUP);    
+  pinMode(buttonPin, INPUT_PULLUP);    //need to do a pullup because of the pin using
 }
 
 void loop(){
@@ -21,14 +23,14 @@ void loop(){
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed.
-  // if it is, the buttonState is LOW:
-  if (buttonState == LOW) {    
+  
+  if (buttonState == LOW) {    //pushbutton is pressed
     // turn LED on:
     digitalWrite(ledPin,LOW);
 
   }
-  else {
-    // turn LED off:
+  else {                      //pushbutton is not pressed
+    // turn LED off
     
     digitalWrite(ledPin,HIGH);
   }
